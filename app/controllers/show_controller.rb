50ae03cea12893e8dc01a6aa669b7c4bd7dd9b0e
ztorres94 <ztorres@chicagobooth.edu>
2021-08-19 00:10:51 +0000

    working on show types

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -5,12 +5,25 @@ def show_type
 
   all_cards = Pokemon::Card
 
-  @list_of_cards = all_cards.where(q: "supertype:#{the_type}")
+  if the_type == "trainer"
+
+    @list_of_cards = all_cards.where(q: "supertype:#{the_type}")
+
+  elsif the_type == "energy"
+    @list_of_cards = all_cards.where(q: "supertype:#{the_type}")
+    
+  else
+
+    @list_of_cards = all_cards.where(q: "type:#{the_type}")
+    
+  end
 
   render({ :template => "show/card_show.html.erb" })
 
   
 end
+
+
 
 def setlist
   the_set = params.fetch("set")
