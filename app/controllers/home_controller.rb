279e913956ef6e396ddb642f35323ef305152d24
ztorres94 <ztorres@chicagobooth.edu>
2021-08-19 02:21:35 +0000

    working on card page next

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
 
 def index
 
+  all_sets = Pokemon::Set.all
+
+  @types = Pokemon::Type.all
+
+  @list_of_sets = all_sets
+  
   render({ :template => "home/index.html.erb" })
 
   
