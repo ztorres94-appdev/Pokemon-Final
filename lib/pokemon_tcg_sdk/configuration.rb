ca40e847b398b4ca5ecb1ffc265200c22317de1e
ztorres94 <ztorres@chicagobooth.edu>
2021-08-21 17:41:21 +0000

    Deleted TCG SDK File

--- lib/pokemon_tcg_sdk/configuration.rb
+++
@@ -1,9 +0,0 @@
-module Pokemon
-  # Placeholder for future configuration options
-  class Configuration
-    attr_accessor :api_key
-
-    def initialize
-    end
-  end
-end
--- lib/pokemon_tcg_sdk/version.rb
+++
@@ -1,3 +0,0 @@
-module Pokemon
-  VERSION = "4.3.0"
-end
